779cabc240c6a0d45e2b70c2aa90032650ebe615
Messicat <112326931+JF195@users.noreply.github.com>
2024-09-07 22:48:22 -0400

    Player controller to a playable and feels-good state

--- Assets/Scripts/TemporaryPlayerController.cs
+++ Assets/Scripts/TemporaryPlayerController.cs
@@ -6,38 +6,63 @@ using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed = 7;
+    [SerializeField] private float movementSpeed = 4;
     [SerializeField] private float jumpForce = 5;
-    private Collider col;
-    private Rigidbody body;
     [SerializeField] private Transform head;
+    [SerializeField] private float coyoteTimeMax = 0.21f;
+    [SerializeField] private float jumpBufferMax = 0.21f;
+    [SerializeField] private float gravityStrength = -9.81f;
+    private float coyoteTime;
+    private float jumpBuffer;
+    private CharacterController controller;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 gravityVelocity = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
         UnityEngine.Cursor.lockState = CursorLockMode.Locked;
-        col = GetComponent<Collider>();
-        body = GetComponent<Rigidbody>();
-    }
-
-    static float GetKey(KeyCode key) 
-    {
-        return Input.GetKey(key) ? 1.0f : 0.0f;
+        controller = GetComponent<CharacterController>();
     }
 
     void Update(){
+        // Handle jump mechanics
+        coyoteTime -= Time.deltaTime;
+        jumpBuffer -= Time.deltaTime;
+
         if (Input.GetKeyDown(KeyCode.Space)){
-            body.velocity += Vector3.up*jumpForce;
+            jumpBuffer = jumpBufferMax;
+        }
+
+        if (jumpBuffer>=0 && coyoteTime>=0){
+            gravityVelocity.y = jumpForce;
         }
     }
-    // Update is called once per frame
+    
     void FixedUpdate()
     {
-        Vector3 inputDir = new(GetKey(KeyCode.D)-GetKey(KeyCode.A), 0, GetKey(KeyCode.W)-GetKey(KeyCode.S));
-        inputDir.Normalize();
+        // Get input direction
+        // (Using "GetAxisRaw" instead of "GetAxis" because "GetAxis" has smoothing applied, which makes the controls feel slightly unresponsive)
+        Vector3 inputDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         Vector3 direction = Quaternion.AngleAxis(head.eulerAngles.y, Vector3.up) * inputDir;
 
-        float amntToReduce = new Vector3(body.velocity.x, 0, body.velocity.z).magnitude;
-        body.drag = amntToReduce;
-        body.AddForce(movementSpeed * direction);
+        // Handle movement (velocity)
+        Vector3 targetVelocity = movementSpeed * direction;
+
+        velocity += (targetVelocity-velocity)/15;
+        if (inputDir.Equals(Vector3.zero)){
+            velocity += (Vector3.zero-velocity)/10;
+        }
+        
+        controller.Move(velocity * Time.fixedDeltaTime);
+
+        // Handle gravity
+        gravityVelocity.y += gravityStrength * Time.fixedDeltaTime * ( (gravityVelocity.y<0) ? 1.65f : 1 );
+        
+        controller.Move(gravityVelocity * Time.fixedDeltaTime);
+
+        if (controller.collisionFlags == CollisionFlags.Below){
+            gravityVelocity.y = 0;
+            coyoteTime = coyoteTimeMax;
+        }
     }
 }
