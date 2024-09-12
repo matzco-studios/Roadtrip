8585cb2f875afded8331fab733104c89ea672337
AchrafGroiez <41470d0a@gmail.com>
2024-09-11 22:42:21 -0400

    Optimized the code by using OnTriggerStay instead of OnTriggerEnter.

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -2,16 +2,9 @@ using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject _ePressMessage;
-
-    [SerializeField]
-    private GameObject _itemContainer;
-
-    private GameObject _nearItem = null;
-
-    [SerializeField]
-    private int _currentSelectedItem = 0;
+    [SerializeField] private GameObject _ePressMessage;
+    [SerializeField] private GameObject _itemContainer;
+    [SerializeField] private int _currentSelectedItem = 0;
 
     private void ShowPressEPanel()
     {
@@ -23,61 +16,64 @@ public class InventoryController : MonoBehaviour
         _ePressMessage.SetActive(false);
     }
 
-    void AddInventoryItem()
+    void AddInventoryItem(GameObject nearItem)
     {
         if (_itemContainer.transform.childCount == 3)
         {
             var itemToDrop = _itemContainer.transform.GetChild(_currentSelectedItem).transform;
             itemToDrop.GetComponent<Rigidbody>().isKinematic = false;
+            nearItem.GetComponent<MeshCollider>().enabled = true;
             itemToDrop.transform.SetParent(null);
         }
 
         else if (_itemContainer.transform.childCount > 0)
         {
-            _nearItem.SetActive(false);
+            nearItem.SetActive(false);
         }
 
-        _nearItem.transform.SetParent(_itemContainer.transform);
+        nearItem.transform.SetParent(_itemContainer.transform);
 
         // Set the scale to (1, 1, 1).
-        _nearItem.transform.localScale = Vector3.one;
+        nearItem.transform.localScale = Vector3.one;
 
         // To do that the item has the same rotation as the parent.
-        _nearItem.transform.localRotation = _itemContainer.transform.localRotation;
+        nearItem.transform.localRotation = _itemContainer.transform.localRotation;
 
-        // Set the postion to (0, 0, 0) to let the child(_nearItem) follow the parent Position(_itemContainer).
-        _nearItem.transform.localPosition = Vector3.zero;
+        // Set the postion to (0, 0, 0) to let the child(nearItem) follow the parent Position(_itemContainer).
+        nearItem.transform.localPosition = Vector3.zero;
 
         // To desactivate RigidBody property.
-        _nearItem.GetComponent<Rigidbody>().isKinematic = true;
+        nearItem.GetComponent<Rigidbody>().isKinematic = true;
+        nearItem.GetComponent<MeshCollider>().enabled = false;
+
+        // Should remove because of OnTriggerExit
         HidePressEPanel();
     }
 
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.E) && _nearItem)
-        {
-            AddInventoryItem();
-        }
-
         ChangeCurrentItem();
     }
 
-    void OnTriggerEnter(Collider other)
+    // The function is executed in loop when two objects are colliding.
+    void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("GrabbableItem"))
         {
-            _nearItem = other.gameObject;
             ShowPressEPanel();
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                AddInventoryItem(other.gameObject);
+            }
         }
 
-        Debug.Log("Entered Trigger");
+        print(other.name);
     }
 
     void OnTriggerExit()
     {
-        _nearItem = null;
-        Debug.Log("Exited Trigger");
+        print("Exited Trigger");
         HidePressEPanel();
     }
 
