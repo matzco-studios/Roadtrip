8a5eeb132754ec2bcc77b68299c56711e62a91ae
Messicat <112326931+JF195@users.noreply.github.com>
2024-09-15 17:40:32 -0400

    Get inside car by press F

---
+++ Assets/Scripts/InteractableCar.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableCar : MonoBehaviour, IInteractable
+{
+    private GameObject _player;
+    private GameObject _car;
+    public string InteractionInfo { get {return "Get in car";} }
+
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _car = transform.parent.gameObject;
+        
+    }
+    void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            OnInteract();
+        }
+    }
+    public void OnInteract()
+    {
+        _player.transform.SetParent(GetComponentInParent<Transform>());
+        _player.transform.localPosition = Vector3.zero;
+        _player.GetComponent<PlayerController>().enabled = false;
+        _player.GetComponent<CharacterController>().enabled = false;
+        _car.GetComponent<CarController>().enabled = true;
+    }
+}
