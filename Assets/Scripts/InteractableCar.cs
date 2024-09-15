c0952f2fff11cef28037dc4ae481f15d6cb3a74e
Messicat <112326931+JF195@users.noreply.github.com>
2024-09-15 19:35:37 -0400

    player can exit car at a specific position

--- Assets/Scripts/InteractableCar.cs
+++ Assets/Scripts/InteractableCar.cs
@@ -7,6 +7,8 @@ public class InteractableCar : MonoBehaviour, IInteractable
 {
     private GameObject _player;
     private GameObject _car;
+    private Transform _exitOffset;
+    private bool _isInCar;
     public string InteractionInfo { get {return "Get in car";} }
 
     void Start()
@@ -14,21 +16,37 @@ public class InteractableCar : MonoBehaviour, IInteractable
         _player = GameObject.FindGameObjectWithTag("Player");
         _car = transform.parent.gameObject;
         _car.GetComponent<CarController>().IsRunning = false;
+        _exitOffset = transform.GetChild(0);
         
     }
-    void Update() 
-    {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            OnInteract();
-        }
-    }
-    public void OnInteract()
+    private void EnterCar()
     {
         _player.transform.SetParent(GetComponentInParent<Transform>());
         _player.transform.localPosition = Vector3.zero;
         _player.GetComponent<PlayerController>().enabled = false;
         _player.GetComponent<CharacterController>().enabled = false;
         _car.GetComponent<CarController>().IsRunning = true;
+        _isInCar = true;
+    }
+    private void ExitCar()
+    {
+        if (_car.GetComponent<Rigidbody>().velocity.magnitude<0.1f)
+        {
+            _player.transform.position = _exitOffset.transform.position;
+            _player.transform.SetParent(null);
+            Vector3 angle = new(0, _car.transform.eulerAngles.y+10, 0);
+            _player.transform.eulerAngles = angle;
+            _player.GetComponent<PlayerController>().enabled = true;
+            _player.GetComponent<CharacterController>().enabled = true;
+            _car.GetComponent<CarController>().IsRunning = false;
+            _isInCar = false;
+        }
+    }
+    public void OnInteract()
+    {
+        if (_isInCar)
+            ExitCar();
+        else
+            EnterCar();
     }
 }
