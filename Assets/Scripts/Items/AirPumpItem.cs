d74d58924414800f37b49853245f36f66a157a5e
Davide Fuoco <2236970@collegemv.qc.ca>
2024-09-24 13:12:40 -0400

    AirPump detects nearby wheel

--- Assets/Scripts/Items/AirPumpItem.cs
+++ Assets/Scripts/Items/AirPumpItem.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AirPumpItem : MonoBehaviour
+public class AirPumpItem : GrabbableItem
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void OnCustomAction()
     {
-        
+        throw new System.NotImplementedException();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnLeftClick()
     {
-        
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnRightClick()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("WheelOfCar"))
+        {
+            print(other.gameObject.name);
+        }
     }
 }
