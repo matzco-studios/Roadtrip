570397584c741434bff866ad39f64bb59b2b2500
Messicat <112326931+JF195@users.noreply.github.com>
2024-09-27 22:46:44 -0400

    fixed key input issues with interaction

--- Assets/Scripts/InteractionController.cs
+++ Assets/Scripts/InteractionController.cs
@@ -4,6 +4,8 @@ public class InteractionController : MonoBehaviour
 {
     [SerializeField] private ActionMessageController _message;
     [SerializeField] private InventoryController _inventory;
+    private int _keyPressE;
+    private int _keyPressF;
 
     private void ForceShowMessage(Collider other)
     {
@@ -27,16 +29,18 @@ public class InteractionController : MonoBehaviour
     {
         if (other.CompareTag("InteractableItem"))
         {
-            if (Input.GetKey(KeyCode.F))
+            if (_keyPressF>0){
                 other.GetComponent<IInteractable>().OnInteract();
-            else
+                _keyPressF = 0;
+            }else
                 ForceShowMessage(other);
         }
         else if (_inventory.Enabled && other.CompareTag("GrabbableItem"))
         {
-            if (Input.GetKeyDown(KeyCode.E))
+            if (_keyPressE>0)
             {
                 _inventory.AddItem(other.gameObject);
+                _keyPressE = 0;
 
                 // Calling OnTriggerExit manually, because it does not activate when we get the item, because we do not leave the trigger zone, 
                 // we just desactivate, the item collider and rigidbody.
@@ -59,7 +63,6 @@ public class InteractionController : MonoBehaviour
     {
         if (_inventory.Enabled)
         {
-
             if (Input.GetKeyDown(KeyCode.Q))
             {
                 _inventory.DropCurrentItem();
@@ -68,5 +71,14 @@ public class InteractionController : MonoBehaviour
             _inventory.ScrollWheelChange();
             _inventory.ChangeCurrentItem();
         }
+        if (Input.GetKeyDown(KeyCode.E))
+            _keyPressE = 2;
+        if (Input.GetKeyDown(KeyCode.F))
+            _keyPressF = 2;
+    }
+    void FixedUpdate()
+    {
+        _keyPressE--;
+        _keyPressF--;
     }
 }
