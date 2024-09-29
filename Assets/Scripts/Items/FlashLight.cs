1a47c1914b127f2507a63f36e6bc5ab8a9d5f65f
AchrafGroiez <41470d0a@gmail.com>
2024-09-29 12:35:44 -0400

    Added ActionDictionnary in GrabbableItem and removed the old abstract functions.

--- Assets/Scripts/Items/FlashLight.cs
+++ Assets/Scripts/Items/FlashLight.cs
@@ -3,11 +3,5 @@ using UnityEngine;
 
 public class FlashLight : GrabbableItem
 {
-    public override void OnCustomAction() { }
-
-    public override void OnLeftClick() { }
-
-    public override void OnRightClick() { }
-
     public FlashLight() : base(Quaternion.Euler(-19.109f, -90, -85.682f)) { }
 }
