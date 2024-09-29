23937f21e633c12692ff1c32a0dfd4f829e44940
AchrafGroiez <41470d0a@gmail.com>
2024-09-29 12:00:20 -0400

    Optimized ActionMessageController by adding a defauly placeholder function.

--- Assets/Scripts/ActionMessageController.cs
+++ Assets/Scripts/ActionMessageController.cs
@@ -38,17 +38,23 @@ public class ActionMessageController : MonoBehaviour
     }
 
     /// <summary>
+    /// Default placeholder function for interacting with an object.
+    /// </summary>
+    public void InteractableItem(string itemName = "with object", string action = "to interact", string key = "F")
+    {
+        _key.text = key;
+        _action.text = action;
+        _itemName.text = itemName;
+        Activate();
+    }
+
+    /// <summary>
     /// Function used by inventory controller to tell the user if he want to grab an item.
     /// </summary>
     /// <param name="item">The item you want to grab.</param>
     public void GrabItem(GameObject item)
     {
-        var itemScript = item.GetComponent<GrabbableItem>();
-        _key.text = "E";
-        _action.text = "to grab";
-        _itemName.text = itemScript && itemScript.Name.Length != 0 ? itemScript.Name : item.name;
-        print($"Displayed the message for {_itemName.text}.");
-        Activate();
+        InteractableItem(itemName: item.GetComponent<GrabbableItem>()?.Name ?? item.name, action: "to grab", key: "E");
     }
 
     /// <summary>
@@ -57,20 +63,6 @@ public class ActionMessageController : MonoBehaviour
     /// <param name="isExit">If the user is trying to exit or enter the car.</param>
     public void CarInteraction(bool isExit = false)
     {
-        _key.text = "F";
-        _action.text = isExit ? "to exit" : "to enter";
-        _itemName.text = "car";
-        Activate();
-    }
-
-    /// <summary>
-    /// Default placeholder function for interacting with an object.
-    /// </summary>
-    public void InteractableItem()
-    {
-        _key.text = "F";
-        _action.text = "to interact";
-        _itemName.text = "with object";
-        Activate();
+        InteractableItem(itemName: "car", action: isExit ? "to exit" : "to enter");
     }
 }
