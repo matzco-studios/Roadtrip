cba8a9f547cedc25610fae101f5391b4903c2372
Messicat <112326931+JF195@users.noreply.github.com>
2024-10-06 16:20:33 -0400

    added method to get message controller

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -11,4 +11,8 @@ public abstract class Interactable : MonoBehaviour
     /// Function that will display a message to the user how he can interact with the object.
     /// </summary>
     public abstract void InteractionMessage();
+
+    public ActionMessageController GetMessage(){
+        return GameObject.FindGameObjectWithTag("ActionMessage").GetComponent<ActionMessageController>();
+    }
 }
