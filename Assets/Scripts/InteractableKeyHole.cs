ef08575c586ee03939e2f645d9dd629cad6691c3
Messicat <112326931+JF195@users.noreply.github.com>
2024-10-06 16:49:01 -0400

    added interactable keyhole script

---
+++ Assets/Scripts/InteractableKeyHole.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableKeyHole : Interactable
+{
+    [SerializeField] private ActionMessageController _message;
+    private CarController _car;
+
+    void Start() {
+        _message = GetMessage();
+        _car = transform.parent.GetComponent<CarController>();
+    }
+
+    public override void InteractionMessage()
+    {
+        if (_car.IsPlayerInside){
+            _message.InteractableItem("car", "to turn " + (_car.IsCarRunning() ? "off" : "on"));
+        }
+    }
+
+    public override void OnInteract()
+    {
+        if (_car.IsPlayerInside){
+            _car.StartEngine();
+            InteractionMessage();
+        }
+    }
+}
