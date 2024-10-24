870567c39543e7ce5c54ebe9f5c3bf392c5338ee
guettafa <irontitou5@gmail.com>
2024-10-24 12:59:34 -0400

    Added CarLight.cs

---
+++ Assets/Scripts/Items/CarLight.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CarLight : GrabbableItem 
+{
+    private float _health;
+
+    public CarLight()
+    {
+        this._health = 100f;
+        Name = "CarLight";
+    }
+}
