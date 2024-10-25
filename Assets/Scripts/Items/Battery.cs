28727b7fa891d0dd2796174500c2da68c731821f
ArturCaa <catar1601@gmail.com>
2024-10-25 14:45:56 -0400

    Added battery collider

--- Assets/Scripts/Items/Battery.cs
+++ Assets/Scripts/Items/Battery.cs
@@ -1,9 +1,33 @@
 using UnityEngine;
 
-public class Battery : GrabbableItem
+public class BatteryPickup : MonoBehaviour
 {
-    public Battery()
+    private Vector3 initialPosition;    // Position d'origine de la batterie
+    private Quaternion initialRotation; // Rotation d'origine de la batterie
+    private bool isPickedUp = false; // État de prise
+    private Transform carHood;
+
+    void Start()
     {
-        Name = "Battery";
+        // Stocke la position et la rotation initiales de la batterie
+        initialPosition = transform.localPosition;
+        initialRotation = transform.localRotation;
+        carHood = transform.parent;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("BatteryCollider"))
+        {
+            transform.SetParent(carHood);
+            transform.localPosition = initialPosition;
+            transform.localRotation = initialRotation;
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+
+    void Update()
+    {
+        
     }
 }
