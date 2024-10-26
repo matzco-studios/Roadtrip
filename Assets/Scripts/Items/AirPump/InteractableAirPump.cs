dbecca34b42bd14537b5141820ad13d0659dad38
JF195 <davidefuoco63@gmail.com>
2024-10-25 21:15:41 -0400

    air pump adds psi to tire

--- Assets/Scripts/Items/AirPump/InteractableAirPump.cs
+++ Assets/Scripts/Items/AirPump/InteractableAirPump.cs
@@ -7,12 +7,14 @@ public class InteractableAirPump : Interactable
     [SerializeField] private ActionMessageController _message;
     private AirPumpItem _airPumpItem;
     private Animation _airPumpAnim;
+    private CarController _car;
 
     void Start()
     {
         _airPumpItem = transform.parent.GetComponent<AirPumpItem>();
         _airPumpAnim = _airPumpItem.GetComponent<Animation>();
         _message = GetMessage();
+        _car = GetCar();
     }
 
     public override void InteractionMessage()
@@ -22,9 +24,13 @@ public class InteractableAirPump : Interactable
 
     public override void OnInteract()
     {
-        if (_airPumpItem.IsConnected)
-            print("Pump air into tire");
-        else 
+        if (_airPumpItem.IsConnected){
+            WheelCollider wheel = _airPumpItem.IsConnected.GetComponent<WheelCollider>();
+            _car.wheels.ForEach(w=>{
+                if (w.wheelCollider==wheel) {w.AddPressure(0.6f);}
+                print(w.Pressure);
+            });
+        }else 
             print("Pump air for nothing");
         _airPumpAnim.Play();
     }
