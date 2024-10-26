b46f2d228add293ed3005ebd4e5f15d5aade0010
AchrafGroiez <41470d0a@gmail.com>
2024-10-25 21:15:13 -0400

    Moved the Wheel class outside of the CarController script.

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,21 +5,6 @@ using UnityEngine.UI;
 
 public class CarController : MonoBehaviour
 {
-    [Serializable]
-    public class Wheel
-    {
-        public const int MaxPsi = 32;
-        public GameObject wheelObject;
-        public WheelCollider wheelCollider;
-        public bool isFrontWheel;
-        private int _pressure = 60;
-        
-        public int Pressure { get => _pressure; }
-        public void FlatTire() => _pressure = 0;
-        public void AddPressure(int amount) => _pressure = Math.Clamp(_pressure + amount, 0, MaxPsi);
-        public void ReducePressure(int amount) => _pressure = Math.Clamp(_pressure - amount, 0, MaxPsi);
-    }
-    
     private Rigidbody rb;
     public List<Wheel> wheels;
     public GameObject steeringWheel;
