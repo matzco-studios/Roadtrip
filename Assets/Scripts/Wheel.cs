4411212155af4feaf25db5084c4642f0447a87c6
JF195 <davidefuoco63@gmail.com>
2024-10-25 21:17:57 -0400

    change ints to floats

--- Assets/Scripts/Wheel.cs
+++ Assets/Scripts/Wheel.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 [Serializable]
 public class Wheel
 {
-    public const int MaxPsi = 32;
+    public const float MaxPsi = 38;
     public GameObject wheelObject;
     public WheelCollider wheelCollider;
     public bool isFrontWheel;
-    private int _pressure = 60;
+    private float _pressure = 33;
         
-    public int Pressure { get => _pressure; }
+    public float Pressure { get => _pressure; }
     public void FlatTire() => _pressure = 0;
-    public void AddPressure(int amount) => _pressure = Math.Clamp(_pressure + amount, 0, MaxPsi);
-    public void ReducePressure(int amount) => _pressure = Math.Clamp(_pressure - amount, 0, MaxPsi);
+    public void AddPressure(float amount) => _pressure = Math.Clamp(_pressure + amount, 0, MaxPsi);
+    public void ReducePressure(float amount) => _pressure = Math.Clamp(_pressure - amount, 0, MaxPsi);
 }
