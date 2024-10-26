0793fe810457de24b809ce18fa4fdde3b6811bdb
guettafa <irontitou5@gmail.com>
2024-10-26 15:41:59 -0400

    Added Light and Particule System attribute to manager with more ease in CarController

--- Assets/Scripts/Items/CarLight.cs
+++ Assets/Scripts/Items/CarLight.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,20 +6,19 @@ namespace Items
 {
     public class CarLight : Mechanics.GrabbableItem
     {
-        private float _health;
-        private Light _light;
-        private Quaternion _initialRotation;
+        public Light ULight; // U Stand for Unity Light
+        public ParticleSystem UFlare;
         
-        /**
-         * Keys are positions of each lights in fornt of the car
-         * -
-         * Values true if they're missing
-         */
-        private KeyValuePair<Vector3, bool> _deadLightsPositions;
+        public bool IsWorking;
+        
+        private Vector3 _initialPosition;
+        private Quaternion _initialRotation;
 
-        public CarLight()
+        public CarLight(Vector3 position)
         {
-            this._health = 100f;
+            _initialPosition = position;
+            
+            IsWorking = true;
             Name = "CarLight";
         }
     }
