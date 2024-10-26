0a2a4cc169f0d6c8b172cfe56398fbfafc3adbdd
AchrafGroiez <41470d0a@gmail.com>
2024-10-26 09:22:27 -0400

    Merged InteractionController.

--- Assets/Scripts/Player/Mechanics/InteractionController.cs
+++ Assets/Scripts/Player/Mechanics/InteractionController.cs
@@ -3,92 +3,106 @@ using UnityEngine;
 
 namespace Player.Mechanics
 {
-    public class InteractionController : MonoBehaviour
+public class InteractionController : MonoBehaviour
+{
+    [SerializeField] private UI.ActionMessageController _message;
+    [SerializeField] private InventoryController _inventory;
+    private int _keyPressE;
+    private int _keyPressF;
+    public GameObject gasPumpOnPlayer;
+
+    private void ForceShowMessage(Collider other)
     {
-        [SerializeField] private UI.ActionMessageController _message;
-        [SerializeField] private InventoryController _inventory;
-        private int _keyPressE;
-        private int _keyPressF;
+        if (!_message.gameObject.activeSelf)
+            OnTriggerEnter(other);
+    }
 
-        private void ForceShowMessage(Collider other)
+    void OnTriggerEnter(Collider collider)
+    {
+        if (_inventory.IsActive() && (collider.CompareTag("GrabbableItem") || collider.CompareTag("Battery")))
         {
-            if (!_message.gameObject.activeSelf)
-                OnTriggerEnter(other);
+            _message.GrabItem(collider.gameObject);
         }
-
-        void OnTriggerEnter(Collider collider)
+        else if (collider.CompareTag("InteractableItem"))
         {
-            if (_inventory.IsActive() && (collider.CompareTag("GrabbableItem") || collider.CompareTag("Battery")))
-            {
-                _message.GrabItem(collider.gameObject);
-            }
-            else if (collider.CompareTag("InteractableItem"))
-            {
-                collider.gameObject.GetComponent<Interactable>().InteractionMessage();
-            }
+            collider.gameObject.GetComponent<Interactable>().InteractionMessage();
         }
-
-        void OnTriggerStay(Collider other)
+        else if (collider.CompareTag("GasPump"))
         {
-            if (other.CompareTag("InteractableItem"))
-            {
-                if (_keyPressF > 0)
-                {
-                    other.GetComponent<Interactable>().OnInteract();
-                    _keyPressF = 0;
-                }
-                else
-                    ForceShowMessage(other);
-            }
-            else if (_inventory.IsActive() && other.CompareTag("GrabbableItem") || other.CompareTag("Battery"))
-            {
-                if (_keyPressE > 0)
-                {
-                    _inventory.AddItem(other.gameObject);
-                    _keyPressE = 0;
-
-                    // Calling OnTriggerExit manually, because it does not activate when we get the item, because we do not leave the trigger zone, 
-                    // we just desactivate, the item collider and rigidbody.
-                    OnTriggerExit(other);
-                }
-                else
-                {
-                    ForceShowMessage(other);
-                }
-            }
-        }
-
-        void OnTriggerExit(Collider other)
-        {
-            if (other.CompareTag("InteractableItem") || other.CompareTag("GrabbableItem"))
-            {
-                _message.SetActive(false);
-            }
-        }
-
-        void Update()
-        {
-            if (_inventory.IsActive())
-            {
-                if (Input.GetKeyDown(KeyCode.Q))
-                {
-                    _inventory.DropCurrentItem();
-                }
-
-                _inventory.ScrollWheelItemChange();
-                _inventory.KeyboardItemChange();
-            }
-
-            if (Input.GetKeyDown(KeyCode.E))
-                _keyPressE = 2;
-            if (Input.GetKeyDown(KeyCode.F))
-                _keyPressF = 2;
-        }
-
-        void FixedUpdate()
-        {
-            _keyPressE--;
-            _keyPressF--;
+            _message.GrabItem(collider.gameObject);
         }
     }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("InteractableItem"))
+        {
+            if (_keyPressF>0){
+                other.GetComponent<Interactable>().OnInteract();
+                _keyPressF = 0;
+            }
+            else
+            {
+                ForceShowMessage(other);
+            }
+        }
+        else if (_inventory.IsActive() && (other.CompareTag("GrabbableItem") || other.CompareTag("Battery")))
+        {
+            if (_keyPressE>0)
+            {
+                _inventory.AddItem(other.gameObject);
+                _keyPressE = 0;
+
+                // Calling OnTriggerExit manually, because it does not activate when we get the item, because we do not leave the trigger zone, 
+                // we just desactivate, the item collider and rigidbody.
+                OnTriggerExit(other);
+            }else{
+                ForceShowMessage(other);
+            }
+        }
+        else if (other.CompareTag("GasPump"))
+        {
+            if (_keyPressE>0)
+            {
+                _inventory.SetActive(false);
+                gasPumpOnPlayer.SetActive(true);
+                other.gameObject.SetActive(false);
+                OnTriggerExit(other);
+            } else {
+                ForceShowMessage(other);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("InteractableItem") || other.CompareTag("GrabbableItem") || other.CompareTag("GasPump"))
+        {
+            _message.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (_inventory.IsActive())
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                _inventory.DropCurrentItem();
+            }
+
+            _inventory.ScrollWheelItemChange();
+            _inventory.KeyboardItemChange();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+            _keyPressE = 2;
+        if (Input.GetKeyDown(KeyCode.F))
+            _keyPressF = 2;
+    }
+    void FixedUpdate()
+    {
+        _keyPressE--;
+        _keyPressF--;
+    }
+}
 }
