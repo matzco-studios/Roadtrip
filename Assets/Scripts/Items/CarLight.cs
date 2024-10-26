9e42a3c13c8d93ea25d8038bc5574cc527987e9f
guettafa <irontitou5@gmail.com>
2024-10-26 14:02:27 -0400

    Added back CarLight.cs

--- Assets/Scripts/Items/CarLight.cs
+++ Assets/Scripts/Items/CarLight.cs
@@ -1,8 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace Items
 {
     public class CarLight : Mechanics.GrabbableItem
     {
         private float _health;
+        private Light _light;
+        private Quaternion _initialRotation;
+        
+        /**
+         * Keys are positions of each lights in fornt of the car
+         * -
+         * Values true if they're missing
+         */
+        private KeyValuePair<Vector3, bool> _deadLightsPositions;
 
         public CarLight()
         {
