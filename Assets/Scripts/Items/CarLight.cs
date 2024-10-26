18d5360a73f3be143ed7625c884cf99e7f28b755
AchrafGroiez <41470d0a@gmail.com>
2024-10-26 13:38:57 -0400

    Revert "Added to Items namespace"
    This reverts commit 0732c95c551aa14d2861d1da1a72428538f9cf72.

--- Assets/Scripts/Items/CarLight.cs
+++ Assets/Scripts/Items/CarLight.cs
@@ -1,16 +1,13 @@
 namespace Items
 {
-    namespace Items
+    public class CarLight : Mechanics.GrabbableItem
     {
-        public class CarLight : Mechanics.GrabbableItem
-        {
-            private float _health;
+        private float _health;
 
-            public CarLight()
-            {
-                this._health = 100f;
-                Name = "CarLight";
-            }
+        public CarLight()
+        {
+            this._health = 100f;
+            Name = "CarLight";
         }
     }
 }
