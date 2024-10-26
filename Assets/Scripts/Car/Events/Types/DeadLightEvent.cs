a2644f68a3dcad3304804d5b8a3a1f08680befd7
guettafa <irontitou5@gmail.com>
2024-10-26 14:00:56 -0400

    Added Back DeadLightEvent.cs

---
+++ Assets/Scripts/Car/Events/Types/DeadLightEvent.cs
@@ -0,0 +1,10 @@
+namespace Car.Events.Types
+{
+    public class DeadLightEvent : CarEvent
+    {
+        public override void Activate()
+        {
+            // Light is dead
+        }
+    }
+}
