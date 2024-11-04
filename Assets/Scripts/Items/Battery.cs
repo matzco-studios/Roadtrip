97425390e9539ba73d13c00fc1b73c3b51d06f9e
JF195 <davidefuoco63@gmail.com>
2024-11-04 09:09:10 -0500

    rename function used

--- Assets/Scripts/Items/Battery.cs
+++ Assets/Scripts/Items/Battery.cs
@@ -34,7 +34,7 @@ namespace Items
         {
             _initialPosition = new Vector3(0.500625f,0.118f,1.547f);
             _initialRotation = transform.localRotation;
-            _carHood = GameObject.FindGameObjectWithTag("Car").transform.FindChild("Body/Body");
+            _carHood = GameObject.FindGameObjectWithTag("Car").transform.Find("Body/Body");
             _carController = GameObject.FindGameObjectWithTag("Car").GetComponent<Car.CarController>();
             _boxColider = GetComponent<BoxCollider>();
             _rigidbody = GetComponent<Rigidbody>();
