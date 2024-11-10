f46d83caa0d2393d35d5f8126056c695340227a3
AchrafGroiez <41470d0a@gmail.com>
2024-11-10 18:58:03 -0500

    Added code to gradually change the weight of the post-processing.

--- Assets/Scripts/Map/Zone.cs
+++ Assets/Scripts/Map/Zone.cs
@@ -6,27 +6,35 @@ namespace Map
 {
     public class Zone : MonoBehaviour
     {
-        [SerializeField] private PostProcessVolume zone;
+        [SerializeField] private PostProcessVolume _ppv;
         private Transform _player;
         private const float ActivationDistance = 0.02f;
         
         private void Start()
         {
             _player = GameObject.FindGameObjectWithTag("Player").transform;
+            _ppv.weight = 0;
         }
+
+        private bool IsInTheZone() => _player.transform.position.z - transform.position.z <= ActivationDistance;
 
         private void Update()
         {
-            if (_player.transform.position.z - transform.position.z <= ActivationDistance)
+            var inZone = IsInTheZone();
+            if (inZone)
             {
                 print("Player is in the zone.");
-                zone.isGlobal = true;
             }
             else
             {
                 print("Player is outside the zone.");
-                zone.isGlobal = false;
             }
+            
+            _ppv.weight = Mathf.Lerp(
+                _ppv.weight, 
+                inZone ? 1f : 0f, 
+                inZone ? 3 * Time.deltaTime : Time.deltaTime / 4
+            );
 
             transform.position = new (transform.position.x, transform.position.y, transform.position.z + 0.75f * Time.deltaTime);
         }
