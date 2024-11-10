d0b4e43be7762195a77bf6561c7e3ee79338b995
AchrafGroiez <41470d0a@gmail.com>
2024-11-09 22:26:30 -0500

    Did that the command move at each frame and activate and desactivate the PostProcess effect.

--- Assets/Scripts/Map/Zone.cs
+++ Assets/Scripts/Map/Zone.cs
@@ -1,10 +1,12 @@
 using System;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 namespace Map
 {
     public class Zone : MonoBehaviour
     {
+        [SerializeField] private PostProcessVolume zone;
         private Transform _player;
         private const float ActivationDistance = 0.76f;
         
@@ -18,7 +20,15 @@ namespace Map
             if (_player.transform.position.z - transform.position.z <= ActivationDistance)
             {
                 print("Player is in the zone.");
+                zone.isGlobal = true;
             }
+            else
+            {
+                print("Player is outside the zone.");
+                zone.isGlobal = false;
+            }
+
+            transform.position = new (transform.position.x, transform.position.y, transform.position.z + 0.75f * Time.deltaTime);
         }
     }
 }
