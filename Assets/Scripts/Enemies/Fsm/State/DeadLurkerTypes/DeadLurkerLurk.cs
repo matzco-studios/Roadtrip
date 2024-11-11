f23da5da199966a6e74203ce688e8a4a8d56658a
JF195 <davidefuoco63@gmail.com>
2024-11-11 13:35:46 -0500

    make wolf go behind the player

--- Assets/Scripts/Enemies/Fsm/State/DeadLurkerTypes/DeadLurkerLurk.cs
+++ Assets/Scripts/Enemies/Fsm/State/DeadLurkerTypes/DeadLurkerLurk.cs
@@ -6,6 +6,10 @@ namespace Enemies.Fsm.State.DeadLurkerTypes
 {
     public class DeadLurkerLurk : Pursue
     {
+        private Vector3 _posBehindPlayer = Vector3.back*5;
+        private Vector3 _posLeftPlayer = Vector3.left*5;
+        private Vector3 _posRightPlayer = Vector3.right*5;
+        private Vector3 _targetPos;
         public DeadLurkerLurk(EnemyController npc, NavMeshAgent agent, Animator anim, Transform player)
             : base(npc, agent, anim, player)
         {
@@ -13,11 +17,18 @@ namespace Enemies.Fsm.State.DeadLurkerTypes
             Agent.speed = 5.0f;
             Agent.isStopped = false;
         }
+        protected override void Enter()
+        {
+            _targetPos = _posBehindPlayer;
+            base.Enter();
+        }
 
         protected override void Update()
         {
-            Npc.transform.LookAt(Player);
-            Agent.SetDestination(Player.position);
+            //Npc.transform.LookAt(Player);
+            var pos = Player.position + Quaternion.AngleAxis(Player.eulerAngles.y, Player.up) * _targetPos;
+            Agent.SetDestination(pos);
+            Debug.DrawLine(Npc.transform.position, pos);
             
             /*if (Npc.CanAttackPlayer)
             {
