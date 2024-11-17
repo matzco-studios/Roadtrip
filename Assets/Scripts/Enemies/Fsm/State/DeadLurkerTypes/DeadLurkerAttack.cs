570737e0541d2b3276ea55d14a506ea833937b4c
JF195 <davidefuoco63@gmail.com>
2024-11-16 21:21:25 -0500

    make deadlurker attack more consistent

--- Assets/Scripts/Enemies/Fsm/State/DeadLurkerTypes/DeadLurkerAttack.cs
+++ Assets/Scripts/Enemies/Fsm/State/DeadLurkerTypes/DeadLurkerAttack.cs
@@ -6,7 +6,7 @@ namespace Enemies.Fsm.State.DeadLurkerTypes
 {
     public class DeadLurkerAttack : Attack
     {
-        private float wait = 160f;
+        private float wait = 180f;
         public DeadLurkerAttack(EnemyController npc, NavMeshAgent agent, Animator anim, Transform player)
             : base(npc, agent, anim, player)
         {
@@ -14,26 +14,19 @@ namespace Enemies.Fsm.State.DeadLurkerTypes
         }
         protected override void Enter()
         {
-            Agent.SetDestination(Player.position);
-            Agent.speed = 3f;
+            Agent.isStopped = true;
             Stage = EventStage.Update;
+            Anim.SetTrigger("StartAttack");
         }
         protected override void Update()
         {
-            if (Agent.remainingDistance<3f && !Agent.isStopped)
-            {
-                Anim.SetTrigger("StartAttack");
-                Agent.isStopped = true;
-                Agent.speed = 0;
-                Agent.ResetPath();
-                Anim.SetBool("IsMoving",false);
-                wait--;
-            }else if (wait<160) {wait-= 1.0f;}
-            Anim.SetFloat("WalkSpeed", Agent.velocity.magnitude);
+            wait-= 1.0f;
+            Anim.SetFloat("WalkSpeed", 0);
             Debug.Log(wait);
             Npc.transform.LookAt(Vector3.Scale(Player.position, new Vector3(1f, 0f, 1f)));
 
             if (wait<=0){
+                Agent.isStopped =false;
                 NextEnemyState = new DeadLurkerIdle(Npc, Agent, Anim, Player);
                 Stage = EventStage.Exit;
                 Debug.Log("Chase");
