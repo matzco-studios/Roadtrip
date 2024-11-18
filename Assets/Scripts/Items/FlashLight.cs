8ab09a4ec35a272a3144073ae45ea700e96203e5
JF195 <davidefuoco63@gmail.com>
2024-11-18 14:05:56 -0500

    add battery to flashlight

--- Assets/Scripts/Items/FlashLight.cs
+++ Assets/Scripts/Items/FlashLight.cs
@@ -4,10 +4,12 @@ namespace Items
 {
     public class FlashLight : Mechanics.GrabbableItem
     {
+        private float _battery = 100f;
         private bool _isTurnedOn = false;
         private Light _light;
         private void LeftMouse()
         {
+            _battery--;
             _isTurnedOn = !_isTurnedOn;
             _light.enabled = _isTurnedOn;
         }
@@ -21,6 +23,18 @@ namespace Items
         {
             _light = transform.GetComponentInChildren<Light>();
             _light.enabled = _isTurnedOn;
+        }
+
+        void Update()
+        {
+            if (_isTurnedOn)
+            {
+                if (_battery<10){
+                    _light.enabled = ! (Mathf.Floor(_battery*300) % Mathf.Ceil(_battery*1.5f)==0);
+                }
+                if (_battery<=0){ _battery += 2.32f; LeftMouse(); }
+                else{ _battery -= Time.deltaTime; }
+            }
         }
     }
 }
