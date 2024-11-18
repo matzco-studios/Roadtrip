fbe0ab9d1564941e7489cf0a9796d47ff1205939
JF195 <davidefuoco63@gmail.com>
2024-11-18 13:57:44 -0500

    add flashlight toggle

--- Assets/Scripts/Items/FlashLight.cs
+++ Assets/Scripts/Items/FlashLight.cs
@@ -4,8 +4,23 @@ namespace Items
 {
     public class FlashLight : Mechanics.GrabbableItem
     {
+        private bool _isTurnedOn = false;
+        private Light _light;
+        private void LeftMouse()
+        {
+            _isTurnedOn = !_isTurnedOn;
+            _light.enabled = _isTurnedOn;
+        }
         public FlashLight() : base(Quaternion.Euler(-19.109f, -90, -85.682f))
         {
+            KeyAction mouseLeft = LeftMouse;
+            ActionDictionary.Add(KeyCode.Mouse0, mouseLeft);
+        }
+
+        void Start()
+        {
+            _light = transform.GetComponentInChildren<Light>();
+            _light.enabled = _isTurnedOn;
         }
     }
 }
