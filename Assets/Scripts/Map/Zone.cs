0a1dfa755378228016321601ac57ae315a068510
AchrafGroiez <41470d0a@gmail.com>
2024-11-19 12:55:15 -0500

    Did that the zone does not need the player in order to work by using a Coroutine that will be started only if the player is not null.

--- Assets/Scripts/Map/Zone.cs
+++ Assets/Scripts/Map/Zone.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Player;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
@@ -10,29 +11,38 @@ namespace Map
         [SerializeField] private PostProcessVolume _ppv;
         private PlayerController _player;
         private const float ActivationDistance = 0.02f;
+        private bool _inZone;
         
         private void Start()
         {
             _player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
             _ppv.weight = 0;
+            if(_player) StartCoroutine(RemovePlayerHealth());
         }
 
         private bool IsInTheZone() => _player.transform.position.z - transform.position.z <= ActivationDistance;
 
+        IEnumerator RemovePlayerHealth()
+        {
+            while (true)
+            {
+                yield return null;
+                _inZone = IsInTheZone();
+                
+                if (_inZone)
+                {
+                    _player.ReduceHealth(2 * Time.deltaTime);
+                    print(_player.Health);
+                }
+            }
+        }
+        
         private void Update()
         {
-            var inZone = IsInTheZone();
-
-            if (inZone)
-            {
-                _player.ReduceHealth(2 * Time.deltaTime);
-                print(_player.Health);
-            }
-            
             _ppv.weight = Mathf.Lerp(
                 _ppv.weight, 
-                inZone ? 1f : 0f, 
-                inZone ? 3 * Time.deltaTime : Time.deltaTime / 4
+                _inZone ? 1f : 0f, 
+                _inZone ? 3 * Time.deltaTime : Time.deltaTime / 4
             );
 
             transform.position = new (transform.position.x, transform.position.y, transform.position.z + 0.75f * Time.deltaTime);
