a1f885dfa410ce60e9ac8b49f20c0f9adce482ac
AchrafGroiez <41470d0a@gmail.com>
2024-11-19 16:01:04 -0500

    Made change to the zone to be able to work with the PlayerScript and added a Speed variable in Zone.

--- Assets/Scripts/Cinematic/EndScene/PlayerScript.cs
+++ Assets/Scripts/Cinematic/EndScene/PlayerScript.cs
@@ -1,4 +1,5 @@
 using System;
+using Map;
 using UnityEngine;
 
 namespace Cinematic.EndScene
@@ -8,17 +9,19 @@ namespace Cinematic.EndScene
         [SerializeField] private float _movementSpeed = 4;
         [SerializeField] private CharacterController _controller;
         private Vector3 _direction;
+        [SerializeField] private Zone _zone;
 
         private void OnEnable()
         {
             transform.position = new (15.024f, 1.08f, transform.position.z);
             transform.SetParent(null);
             _controller.enabled = true;
+            _zone.Speed = 5f;
         }
 
         private void Movement()
         {
-            _controller.Move(transform.TransformDirection(new Vector3(0, 0f, -0.1f)));
+            _controller.Move(transform.TransformDirection(new Vector3(0, 0f, -2f * Time.deltaTime)));
         }
 
         void FixedUpdate()
--- Assets/Scripts/Map/Zone.cs
+++ Assets/Scripts/Map/Zone.cs
@@ -9,15 +9,18 @@ namespace Map
     public class Zone : MonoBehaviour
     {
         [SerializeField] private PostProcessVolume _ppv;
-        private PlayerController _player;
+        [SerializeField] private GameObject _player;
+        private PlayerController _playerController;
         private const float ActivationDistance = 0.02f;
         private bool _inZone;
+        public float Speed = 0.75f;
         
         private void Start()
         {
-            _player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+            _player = GameObject.FindGameObjectWithTag("Player");
+            _playerController = _player.GetComponent<PlayerController>();
             _ppv.weight = 0;
-            if(_player) StartCoroutine(RemovePlayerHealth());
+            if(_playerController) StartCoroutine(RemovePlayerHealth());
         }
 
         private bool IsInTheZone() => _player.transform.position.z - transform.position.z <= ActivationDistance;
@@ -27,25 +30,26 @@ namespace Map
             while (true)
             {
                 yield return null;
-                _inZone = IsInTheZone();
                 
                 if (_inZone)
                 {
-                    _player.ReduceHealth(2 * Time.deltaTime);
-                    print(_player.Health);
+                    _playerController.ReduceHealth(2 * Time.deltaTime);
+                    print(_playerController.Health);
                 }
             }
         }
         
         private void Update()
         {
+            _inZone = IsInTheZone();
+
             _ppv.weight = Mathf.Lerp(
                 _ppv.weight, 
                 _inZone ? 1f : 0f, 
                 _inZone ? 3 * Time.deltaTime : Time.deltaTime / 4
             );
 
-            transform.position = new (transform.position.x, transform.position.y, transform.position.z + 0.75f * Time.deltaTime);
+            transform.position = new (transform.position.x, transform.position.y, transform.position.z + Speed * Time.deltaTime);
         }
     }
 }
