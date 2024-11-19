d9bab3f15b0ea3a85139e498872d3d68c7bbbc8c
AchrafGroiez <41470d0a@gmail.com>
2024-11-19 17:44:32 -0500

    Added a transition for the rotation of the camera when the player is inside the zone.

--- Assets/Scripts/Cinematic/EndScene/CameraScript.cs
+++ Assets/Scripts/Cinematic/EndScene/CameraScript.cs
@@ -1,14 +1,31 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Cinematic.EndScene
 {
     public class CameraScript : MonoBehaviour
     {
+        private float _rotation;
+        private const float MaxRotation = -90f;
+        private const float RotationDelta = 50f;
+
         private void OnEnable()
         {
             transform.SetParent(null);
-            transform.Rotate(270, 0, 0);
+            StartCoroutine(ChangeRotation());
+        }
+
+        private IEnumerator ChangeRotation()
+        {
+            while (_rotation > MaxRotation)
+            {
+                _rotation = Mathf.MoveTowards(_rotation, MaxRotation, RotationDelta * Time.deltaTime);
+                transform.rotation = Quaternion.Euler(_rotation, 180f, 0f);
+                yield return null;
+            }
+
+            yield return null;
         }
     }
 }
