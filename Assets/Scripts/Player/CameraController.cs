bea151787c6015fb4e1da5764c929545ec798d6a
JF195 <davidefuoco63@gmail.com>
2024-11-22 19:47:02 -0500

    add recoil code

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -7,12 +7,22 @@ namespace Player
         [SerializeField] private float _sensitivity = 2.0f;
         [SerializeField] private Transform _head;
         private float _verticalRotation = 0;
+        private Vector2 _recoil;
+
+        public void ApplyRecoilEffect(float amnt){
+            _recoil.y += amnt;
+        }
 
         void Update()
         {
             // Rotate camera and head
             float mouseX = Input.GetAxis("Mouse X") * _sensitivity;
             float mouseY = Input.GetAxis("Mouse Y") * _sensitivity;
+
+            mouseX += _recoil.x;
+            mouseY += _recoil.y;
+
+            _recoil = Vector2.Lerp(_recoil, Vector2.zero, Time.deltaTime*50);
 
             _verticalRotation = Mathf.Clamp(_verticalRotation - mouseY, -90f, 90f);
 
