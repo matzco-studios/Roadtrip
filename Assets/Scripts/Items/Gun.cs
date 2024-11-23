24190a25f0e81e611e38ffa8fedf7338482414e6
JF195 <davidefuoco63@gmail.com>
2024-11-22 20:10:57 -0500

    basic gun functionality

--- Assets/Scripts/Items/Gun.cs
+++ Assets/Scripts/Items/Gun.cs
@@ -1,5 +1,7 @@
 using System;
 using Items.Mechanics;
+using Player;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Items
@@ -10,5 +12,47 @@ namespace Items
         private int _magSize = 7;
         [SerializeField]
         private float _damage = 13f;
+        [SerializeField]
+        private float _recoil = .8f;
+        [SerializeField]
+        private float _cooldown = .425f;
+        private CameraController _cameraController;
+        private Animator _animator;
+        private int _magazine;
+        private float _shootCooldown;
+        private bool isReloading = false;
+
+        private void Shoot(){
+            if (_shootCooldown<0 && _magazine>0)
+            {
+                _cameraController.ApplyRecoilEffect(_recoil);
+                _shootCooldown = _cooldown;
+                _magazine--;
+                isReloading = false;
+                _animator.SetTrigger("Shoot");
+            }
+        }
+
+        private void Reload(){
+            isReloading = true;
+            _animator.SetTrigger("Reload");
+        }
+
+        public Gun()
+        {
+            ActionDictionary.Add(KeyCode.Mouse0, Shoot);
+            ActionDictionary.Add(KeyCode.R, Reload);
+        }
+
+        void Start(){
+            _cameraController = GameObject.FindGameObjectWithTag("Player").GetComponent<CameraController>();
+            _magazine = _magSize;
+            _animator =GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            _shootCooldown -= Time.deltaTime;
+        }
     }
 }
