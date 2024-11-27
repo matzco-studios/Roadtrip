8ed9f841d808f86c2b76d6427274fd7d9ef5c30f
JF195 <davidefuoco63@gmail.com>
2024-11-27 13:22:31 -0500

    scorchlet jumpscare goes back to main menu

--- Assets/Scripts/Enemies/Scorchlet/ScorchletJumpscare.cs
+++ Assets/Scripts/Enemies/Scorchlet/ScorchletJumpscare.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Enemies.Scorchlet
 {
     public class ScorchletJumpscare : MonoBehaviour
     {
         private Animator anim;
+        private float timer = 0;
         // Start is called before the first frame update
         void Start()
         {
@@ -19,10 +21,12 @@ namespace Enemies.Scorchlet
         // Update is called once per frame
         void Update()
         {
-            if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.85f)
-            {
-                Destroy(gameObject);
-            }
+            if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.7f && timer==0)
+            { timer = 1; Destroy(gameObject.transform.GetChild(1).gameObject); }
+
+            if (timer>0){ timer += Time.deltaTime; }
+
+            if (timer>3){ SceneManager.LoadScene(0); }
         }
     }
 }
