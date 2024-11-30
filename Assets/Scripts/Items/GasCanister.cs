47ec2a3dde7e80f0ab44bd98819ce391a17d107a
QuocDungTran380 <kevintran380@gmail.com>
2024-11-30 18:10:52 -0500

    fuel canister final

--- Assets/Scripts/Items/GasCanister.cs
+++ Assets/Scripts/Items/GasCanister.cs
@@ -14,51 +14,60 @@ namespace Items
         private Quaternion _pickedRotation;
         private Transform fuelTank;
         private GameObject _pickedParent;
-        private GameObject _initialParent;
-        private Vector3 _initialPosition;
-        private Vector3 _initialRotation;
         private Rigidbody rb;
+        private float _fuelAmount;
+        private BoxCollider triggerBox;
+
 
         void Start()
         {
-            _initialPosition = transform.position;
-            _initialRotation = transform.rotation.eulerAngles;
+            _fuelAmount = Random.Range(0.1f, 0.5f);
             _isPicked = false;
             _pickedPosition = new Vector3(0, 0, 0);
             _pickedParent = null;
-            _initialParent = transform.parent.gameObject;
             rb = GetComponent<Rigidbody>();
+            triggerBox = GetComponent<BoxCollider>();
         }
 
         void OnTriggerStay(Collider other)
         {
             if (other.gameObject.CompareTag("FuelTank"))
             {
-                _fillingPosition = new Vector3(1.25f, -0.15f, -0.26f);
-                _fillingRotation = Quaternion.Euler(0, -90, 0);
+                _fillingPosition = new Vector3(1.25f, 0.5f, -0.26f);
+                _fillingRotation = Quaternion.Euler(0, 0, 118);
                 fuelTank = GameObject.FindGameObjectWithTag("FuelTank").transform;
                 if (Input.GetKey(KeyCode.E))
                 {
-                    transform.SetParent(fuelTank);
-                    transform.SetLocalPositionAndRotation(_fillingPosition, _fillingRotation);
-                    GameObject.FindGameObjectWithTag("Car").GetComponent<CarController>().Refuel(0.1f);
+                    if (_fuelAmount > 0)
+                    {
+                        triggerBox.excludeLayers = LayerMask.GetMask("Ignore Raycast");
+                        transform.SetParent(fuelTank);
+                        transform.SetLocalPositionAndRotation(_fillingPosition, _fillingRotation);
+                        GameObject.FindGameObjectWithTag("Car").GetComponent<CarController>().Refuel(0.1f);
+                        _fuelAmount -= 0.1f * Time.deltaTime;
+                    }
+                    else
+                    {
+                        ResetPosition();
+                    }
                 }
                 else if (rb.isKinematic)
                 {
-                    transform.SetLocalPositionAndRotation(_pickedPosition, _pickedRotation);
-                    transform.SetParent(_pickedParent.transform);
+                    ResetPosition();
                 }
             }
-            else if (other.gameObject.CompareTag("GasMachine") && !_isPicked && (transform.parent == null || transform.parent ==_initialParent.transform))
-            {
-                transform.SetPositionAndRotation(_initialPosition, Quaternion.Euler(_initialRotation));
-                transform.SetParent(_initialParent.transform);
-                rb.constraints = RigidbodyConstraints.FreezeAll;
-            }
+        }
+
+        void ResetPosition()
+        {
+            triggerBox.excludeLayers = LayerMask.GetMask();
+            transform.SetLocalPositionAndRotation(_pickedPosition, _pickedRotation);
+            transform.SetParent(_pickedParent.transform);
         }
 
         void FixedUpdate()
         {
+            Debug.Log(_fuelAmount);
             if (transform.parent != null && transform.parent.gameObject != null)
             {
                 if (gameObject.transform.parent.gameObject.name == "ItemContainer")
