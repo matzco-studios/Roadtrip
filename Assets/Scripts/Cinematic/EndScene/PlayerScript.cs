fe67a9bb3ae0b741bfcd0c56fd8aee9a7c23fe60
AchrafGroiez <41470d0a@gmail.com>
2024-11-30 10:38:51 -0500

    Added Brian character with sitting and running animations and changed camera zoom in EndScenePrototype.

--- Assets/Scripts/Cinematic/EndScene/PlayerScript.cs
+++ Assets/Scripts/Cinematic/EndScene/PlayerScript.cs
@@ -7,21 +7,23 @@ namespace Cinematic.EndScene
 {
     public class PlayerScript : MonoBehaviour
     {
-        [SerializeField] private float _movementSpeed = 4;
+        [SerializeField] private float _movementSpeed = 5f;
         [SerializeField] private CharacterController _controller;
-        private Vector3 _direction;
         [SerializeField] private Zone _zone;
         [SerializeField] private CameraScript _camera;
+        private Animator _animator;
         private bool _stopped;
         
         public void StopPlayer() => _stopped = true;
         
         private void OnEnable()
         {
+            _animator = GetComponent<Animator>();
+            _animator.enabled = true;
             transform.SetParent(null);
             transform.position = new (transform.position.x - 2f, transform.position.y, transform.position.z);
             _controller.enabled = true;
-            _zone.Speed = 5f;
+            _zone.Speed = 7f;
             StartCoroutine(PlayerMovement());
         }
 
@@ -30,11 +32,12 @@ namespace Cinematic.EndScene
             while (!_stopped)
             {
                 yield return null;
-                _controller.Move(transform.TransformDirection(new Vector3(0, 0f, -2f * Time.deltaTime)));
+                _controller.Move(transform.TransformDirection(new Vector3(0, 0f, _movementSpeed * Time.deltaTime)));
                 
                 if (_zone.IsInTheZone() && !_camera.enabled)
                 {
                     _camera.enabled = true;
+                    _animator.enabled = false;
                 }
             }
         }
