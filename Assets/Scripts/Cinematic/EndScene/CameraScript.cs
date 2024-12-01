8b9291746b412c6d57ce52a40638a163fc33d14a
AchrafGroiez <41470d0a@gmail.com>
2024-11-30 23:11:12 -0500

    Added multi color bars image.

--- Assets/Scripts/Cinematic/EndScene/CameraScript.cs
+++ Assets/Scripts/Cinematic/EndScene/CameraScript.cs
@@ -8,6 +8,7 @@ namespace Cinematic.EndScene
     {
         [SerializeField] private GameObject _brokenEffect;
         [SerializeField] private AudioSource _brokenSound;
+        [SerializeField] private GameObject _multiColorBars;
         private Zone _zone;
         private Animator _animator;
         private bool _endRotation;
@@ -37,6 +38,9 @@ namespace Cinematic.EndScene
             _brokenEffect.SetActive(true);
             _zone.Stop();
             _player.Stop();
+            yield return new WaitForSeconds(1f);
+            _multiColorBars.SetActive(true);
+            _brokenEffect.SetActive(false);
         }
     }
 }
