1238c58f8e1c44144b6c1f3ae71c0865be05f88e
guettafa <irontitou5@gmail.com>
2024-12-02 22:05:15 -0500

    Added SunburnedEnemy.cs

--- Assets/Scripts/Enemies/Sunburned/SunburnedEnemy.cs
+++ Assets/Scripts/Enemies/Sunburned/SunburnedEnemy.cs
@@ -1,18 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using Animator = UnityEngine.Animator;
 
-public class SunburnedEnemy : MonoBehaviour
+
+namespace Enemies.Sunburned
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SunburnedEnemy : EnemyController
     {
+        [SerializeField] private PostProcessVolume postProcess;
+        [SerializeField] private float activationDistance;
         
-    }
+        private Renderer _renderer;
+        private Animator _animator;
+        
+        private bool _isWatched;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Awake()
+        {
+            _renderer = GetComponent<Renderer>();
+            _animator = transform.GetChild(1).GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            _isWatched = _renderer.isVisible;
+            
+            /*
+             * Check if player is looking at the sunburned
+             * if no, Sunburned is looking at the player & move towards it  
+             */
+            if (!_isWatched) 
+            {
+                transform.LookAt(new Vector3(Target.position.x, transform.position.y, Target.position.z));
+                transform.position = Vector3.MoveTowards(transform.position, Target.position, speed * Time.deltaTime);
+            } 
+            _animator.enabled = !_isWatched;
+            
+            /*
+             * Check if player is near, if yes,
+             * make use of the Post Process effect
+             */
+            if (Vector3.Distance(Target.position, transform.position) < activationDistance) {
+                postProcess.weight = Mathf.Lerp(
+                    postProcess.weight,
+                    (_isWatched ? 1f : 0f),
+                    speed * Time.deltaTime
+                );
+            }
+
+        }
     }
 }
