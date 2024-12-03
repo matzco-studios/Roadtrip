cccb25280788b04cd6727f025e0e1577826876b8
AchrafGroiez <41470d0a@gmail.com>
2024-12-03 16:10:22 -0500

    Fixed the right scene index in MainMenu.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,7 +9,7 @@ public class MainMenu : MonoBehaviour
     }
     public void PlayGame()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame()
