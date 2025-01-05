d040d03b82753801dae1f4b085139cca5c14d56c
AchrafGroiez <41470d0a@gmail.com>
2025-01-05 00:23:52 -0500

    Moved default spawn in Level1 and fixed bug with radio trigger.

--- Assets/Scripts/Cinematic/LevelOne/RadioTrigger.cs
+++ Assets/Scripts/Cinematic/LevelOne/RadioTrigger.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class RadioTrigger : MonoBehaviour
 {
     private AudioSource _radioSound;
+    private bool hasPlayed = false;
 
     private void Start() {
         _radioSound = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other) {
-        if(other.CompareTag("Car")) {
+        if(other.CompareTag("Car") && !hasPlayed) {
+            hasPlayed = true;
             _radioSound.Play();
         }
     }
